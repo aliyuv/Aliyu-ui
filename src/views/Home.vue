2cf29df91c36cc78b31e735fd13d81a3cacccd61
aliyu <1324149139@qq.com>
2022-05-29 14:28:04 +0800

    完成Home页面的CSS样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>咕噜UI</h1>
       <h2>一个厉害的UI框架</h2>
       <p class="actions">
-        <a href="#">GitHub</a>
-        <a href="#">开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -22,5 +22,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
 
+  > .actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
