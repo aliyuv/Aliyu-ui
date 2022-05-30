8b92ad27b50e978a210e63907c0b59d08d014db6
aliyu <1324149139@qq.com>
2022-05-30 12:30:49 +0800

    动画流畅了很多

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -37,6 +37,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 
 button.checked {
@@ -45,5 +46,9 @@ button.checked {
 
 button.checked:hover > span {
   left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+  outline: none;
 }
 </style>
