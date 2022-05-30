49b02c13169975e88714a72aba5918222acf2c10
aliyu <1324149139@qq.com>
2022-05-30 17:38:38 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -14,21 +14,24 @@ export default {
     },
     size: {
       type: String,
-      default: "normal"
-    }
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`gulu-theme-${theme}`]: theme,
-        [`gulu-size-${size}`]: size
+        [`gulu-size-${size}`]: size,
+        [`gulu-level-${level}`]: level,
       };
     });
-    return {
-      classes
-    };
-  }
+    return {classes};
+  },
 };
 </script>
 <style lang="scss">
@@ -37,6 +40,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .gulu-button {
   box-sizing: border-box;
   height: $h;
@@ -51,6 +55,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -75,7 +80,8 @@ $radius: 4px;
     box-shadow: none;
     color: $blue;
 
-    &:hover, &:focus {
+    &:hover,
+    &:focus {
       color: lighten($blue, 10%);
     }
   }
@@ -85,22 +91,78 @@ $radius: 4px;
     box-shadow: none;
     color: inherit;
 
-    &:hover, &:focus {
-      background: darken(white, 5%);;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
     }
   }
 
+  &.gulu-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.gulu-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
   &.gulu-theme-button {
-    &.gulu-size-big {
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px
+    &.gulu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
 
-    &.gulu-size-small {
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+    &.gulu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.gulu-theme-link {
+    &.gulu-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.gulu-theme-text {
+    &.gulu-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.gulu-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
 }
