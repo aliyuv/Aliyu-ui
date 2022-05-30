a1895a12686740264d1d96d147e5a04a473bfe67
aliyu <1324149139@qq.com>
2022-05-30 17:30:58 +0800

    支持size

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,21 +1,36 @@
 <template>
-  <button class="gulu-button" :class="{[`gulu-theme-${theme}`]:theme}">
+  <button class="gulu-button" :class="classes">
     <slot/>
   </button>
 </template>
-
 <script lang="ts">
+import {computed} from "vue";
+
 export default {
-  name: "Button",
   props: {
     theme: {
       type: String,
-      default: "button"
+      default: "button",
+    },
+    size: {
+      type: String,
+      default: "normal"
     }
+  },
+  setup(props) {
+    const {theme, size} = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size
+      };
+    });
+    return {
+      classes
+    };
   }
 };
 </script>
-
 <style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
@@ -53,6 +68,40 @@ $radius: 4px;
 
   &::-moz-focus-inner {
     border: 0;
+  }
+
+  &.gulu-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.gulu-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);;
+    }
+  }
+
+  &.gulu-theme-button {
+    &.gulu-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+
+    &.gulu-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
   }
 }
 </style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -43,34 +43,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-aside {
-  background: white;
-  width: 150px;
-  padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
-  height: 100%;
-
-  > h2 {
-    margin-bottom: 4px;
-  }
-
-  > ol {
-    > li {
-      padding: 4px 0;
-    }
-  }
-
-  @media (max-width: 500px) {
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
-  }
-}
-
 .layout {
   display: flex;
   flex-direction: column;
@@ -100,8 +72,33 @@ aside {
   > main {
     flex-grow: 1;
     padding: 16px;
-    background: lightgreen;
+    background: white;
   }
+}
+
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
+
+  > h2 {
+    margin-bottom: 4px;
+  }
+
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+
+main {
+  overflow: auto;
 }
 </style>
 
