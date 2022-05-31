f560aa24948244e6b68a92bed670036bd2dc2acc
aliyu <1324149139@qq.com>
2022-05-31 12:11:38 +0800

    Dialog 支持 visible

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,33 +1,45 @@
 <template>
-  <div class="gulu-dialog-overlay"></div>
-  <div class="gulu-dialog-wrapper">
-    <div class="gulu-dialog">
-      <header>标题 <span class="gulu-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button leave="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="gulu-dialog-overlay"></div>
+    <div class="gulu-dialog-wrapper">
+      <div class="gulu-dialog">
+        <header>
+          标题
+          <span class="gulu-dialog-close"></span>
+        </header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from "./Button.vue";
 
 export default {
-  name: "Dialog",
-  components: {Button}
-
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: {
+    Button,
+  },
 };
 </script>
 
 <style lang="scss">
 $radius: 4px;
 $border-color: #d9d9d9;
+
 .gulu-dialog {
   background: white;
   border-radius: $radius;
@@ -97,6 +109,7 @@ $border-color: #d9d9d9;
     &::after {
       transform: translate(-50%, -50%) rotate(45deg);
     }
+
   }
 }
 </style>
