156bc8b0b6819a5076e82429366d81cb62bd8a01
aliyu <1324149139@qq.com>
2022-05-31 13:31:19 +0800

    使用 Teleport

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,16 +1,19 @@
 <template>
   <div>Dialog 示例</div>
   <h1>示例1</h1>
-  <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :close-click-overlay="false" :ok="f1" :cancel="f2">
-    <template v-slot:title>
-      <strong>加错的标题</strong>
-    </template>
-    <template v-slot:content>
-      <strong>hi</strong>
-      <div>hi2</div>
-    </template>
-  </Dialog>
+  <div style="position: relative; z-index: 1">
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :close-click-overlay="false" :ok="f1" :cancel="f2">
+      <template v-slot:title>
+        <strong>加错的标题</strong>
+      </template>
+      <template v-slot:content>
+        <strong>hi</strong>
+        <div>hi2</div>
+      </template>
+    </Dialog>
+  </div>
+  <div style="position: relative; z-index: 1; width: 300px; height: 300px; background: red;"></div>
 </template>
 
 <script lang="ts">
