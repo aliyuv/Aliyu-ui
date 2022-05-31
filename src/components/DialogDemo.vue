8f07fd27dbba7b1bfb20bba138d237f84b0f1197
aliyu <1324149139@qq.com>
2022-05-31 13:23:06 +0800

    Dialog 支持自定义 title 和 content

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,15 @@
   <div>Dialog 示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :close-click-overlay="false" :ok="f1" :cancel="f2"></Dialog>
+  <Dialog v-model:visible="x" :close-click-overlay="false" :ok="f1" :cancel="f2">
+    <template v-slot:title>
+      <strong>加错的标题</strong>
+    </template>
+    <template v-slot:content>
+      <strong>hi</strong>
+      <div>hi2</div>
+    </template>
+  </Dialog>
 </template>
 
 <script lang="ts">
