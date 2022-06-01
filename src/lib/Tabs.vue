3d75dad6020719651c8cbf01f58c63d7f7fde971
aliyu <1324149139@qq.com>
2022-06-01 13:05:16 +0800

    添加Tabs 样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,12 @@
 <template>
-  <div v-for="(t,index) in titles">{{t}}</div>
-  <component v-for="(c,index) in defaults" :is="c" :key="index"></component>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -21,11 +27,38 @@ export default {
     return {
       defaults,
       titles
-    }
+    };
   }
 };
 </script>
 
-<style scoped>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
 
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
 </style>
