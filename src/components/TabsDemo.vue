b7f7f49c0363c068cdcd7c3918ed63c6fe3dc013
aliyu <1324149139@qq.com>
2022-06-01 14:36:39 +0800

    基本完成Tabs组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,6 +1,7 @@
 <template>
   <div>Tabs 示例</div>
-  <Tabs>
+  <h1>示例1</h1>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
@@ -9,13 +10,20 @@
 <script lang="ts">
 import Tabs from "../lib/Tabs.vue";
 import Tab from "../lib/Tab.vue";
+import {
+  ref
+} from "vue";
 
 export default {
-  name: "TabsDemo",
-  components: {Tabs, Tab}
+  components: {
+    Tabs,
+    Tab
+  },
+  setup() {
+    const x = ref("导航1");
+    return {
+      x
+    };
+  }
 };
 </script>
-
-<style scoped>
-
-</style>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,32 +1,54 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}"
+           :key="index">{{ t }}
+      </div>
+      <div class="gulu-tabs-nav-indicator"></div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults"
+                 :is="c"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from "./Tab.vue";
+import {
+  computed
+} from "vue";
 
 export default {
-  name: "Tabs",
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
-        throw new Error("Tabs 子组件必须是Tab");
+        throw new Error("Tabs 子标签必须是 Tab");
       }
+    });
+    const current = computed(() => {
+      console.log("重新 return");
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
     });
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
     return {
       defaults,
-      titles
+      titles,
+      current,
+      select
     };
   }
 };
@@ -36,11 +58,13 @@ export default {
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
+
 .gulu-tabs {
   &-nav {
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -55,10 +79,27 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+    }
   }
 
   &-content {
     padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
