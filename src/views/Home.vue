f7da44c54cf9b0f76958c52d477d4f06f89c208c
aliyu <1324149139@qq.com>
2022-06-02 08:38:58 +0800

    从 iconfont.cn 引入图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="topnavAndBanner">
     <Topnav/>
     <div class="banner">
       <h1>咕噜UI</h1>
@@ -9,6 +9,17 @@
         <router-link to="/doc">开始</router-link>
       </p>
     </div>
+  </div>
+  <div class="features">
+    <svg class="icon">
+      <use xlink:href="#icon-Vue"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-typescript"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-lights"></use>
+    </svg>
   </div>
 </template>
 
@@ -22,26 +33,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.topnavAndBanner {
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
+
+.features {
+  > svg {
+    width: 64px;
+    height: 64px;
+  }
+}
+
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
 
   > .actions {
     padding: 8px 0;
 
     a {
       margin: 0 8px;
-      background: #fff;
+      background: $green;
+      color: #fff;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
