7e5bee6abe73dbf87f0a66b9bb6a590464c6ae70
aliyu <1324149139@qq.com>
2022-06-03 10:10:32 +0800

    添加Markdown说明

---
+++ src/markdown/install.md
@@ -0,0 +1,15 @@
+# 安装
+
+打开终端运行下列命令：
+
+```
+npm install aliyu-ui
+```
+
+或
+
+```
+yarn add aliyu-ui
+```
+
+下一节：[开始使用](#/doc/get-started)
--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,20 +1,17 @@
 <template>
-  <article class="markdown-body">
-    <h1>安装</h1>
-    <p>打开终端运行下列命令：</p>
-    <pre><code>npm install aliyu-ui</code></pre>
-    <p>或</p>
-    <pre><code>yarn add aliyu-ui</code></pre>
-
-    <p>
-      下一节：<a href="#/doc/get-started">开始使用</a>
-    </p>
-  </article>
+  <article class="markdown-body" v-html="md"></article>
 </template>
 
 <script>
+import md from "../markdown/install.md";
+
 export default {
-  name: "Install"
+  name: "Install",
+  data() {
+    return {
+      md
+    }
+  }
 }
 </script>
 
